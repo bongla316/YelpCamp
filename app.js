719bb2a3d877501109c4fc6b55ea88652d0e2593
bongla316 <abdullah.safdar@gmail.com>
2018-09-04 16:49:56 +0100

    Refactored app.js to move campground and comment models into separate folder

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
-var express = require("express"),
-   app = express(),
-   bodyParser = require("body-parser"),
-   mongoose = require("mongoose");
+var   express     = require("express"),
+      app         = express(),
+      bodyParser  = require("body-parser"),
+      mongoose    = require("mongoose"),
+      Campground  = require("./models/campground"),
+      seedDB      = require("./seeds");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", {
    useNewUrlParser: true
 });
 app.use(bodyParser.urlencoded({
    extended: true
 }));
 app.set("view engine", "ejs");
-
-var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String,
-   description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create({
-//       name: "Granit Hill",
-//       image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-//       description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
-//    },
-//    function (err, campground) {
-//       if (err) {
-//          console.log(err);
-//       } else {
-//          console.log("Newly created campground: ");
-//          console.log(campground);
-//       }
-//    }
-// );
+seedDB();
 
 app.get('/', function (req, res) {
    res.render("landing");
@@ -82,7 +62,7 @@ app.post("/campgrounds", function (req, res) {
 // SHOW Route - Shows info about one campground (/campgrounds/:id)
 app.get("/campgrounds/:id", function (req, res) {
    // Find campground with provided ID
-   Campground.findById(req.params.id, function(err, foundCampground){
+   Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
       if(err) {
          console.log(err);
       } else {
