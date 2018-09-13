99b9a79a19e71599789bad82beeaab24e5e8324c
bongla316 <abdullah.safdar@gmail.com>
2018-09-13 18:25:53 +0100

    Added method to associate a newly created campground with a user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,27 +23,32 @@ router.get('/', function (req, res) {
 });
 
 // NEW - show form to create new campground
-router.get("/new", function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
    res.render("campgrounds/new");
 });
 
 // CREATE Route - add new campground to database and display it
-router.post("/", function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
    // Get data from form and add to database
    var name = req.body.name;
    var image = req.body.image;
    var description = req.body.description;
 
+   var author = {
+      id: req.user._id,
+      username: req.user.username
+   };
    var newCampground = {
       name: name,
       image: image,
-      description: description
+      description: description,
+      author: author
    };
-
-   Campground.create(newCampground, function (err, campground) {
+   Campground.create(newCampground, function (err, campgroundAdded) {
       if (err) {
          console.log(err);
       } else {
+         console.log(campgroundAdded);
          res.redirect("/campgrounds");
       }
    });
@@ -63,5 +68,13 @@ router.get("/:id", function (req, res) {
       }
    });
 });
+
+// Middleware for logout route
+function isLoggedIn(req, res, next){
+   if(req.isAuthenticated()){
+      return next();
+   }
+   return res.redirect("/login");
+}
 
 module.exports = router;
