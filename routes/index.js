ee4a14fe3495037c13cfa56684c847fd19840ea5
bongla316 <abdullah.safdar@gmail.com>
2018-09-18 17:43:30 +0100

    Improved error handling to be more useful for users

--- routes/index.js
+++ routes/index.js
@@ -29,11 +29,12 @@ router.post("/register", function (req, res) {
    });
    User.register(newUser, req.body.password, function (err, user) {
       if (err) {
-         console.log(err);
-         return res.render("register");
+         req.flash("error", err.message);
+         return res.redirect("register");
       }
 
       passport.authenticate("local")(req, res, function () {
+         req.flash("success", `Welcome to YelpCamp ${user.username}!`);
          res.redirect("/campgrounds");
       });
    });
@@ -55,6 +56,7 @@ router.post("/login", passport.authenticate("local", {
 // Logout route
 router.get("/logout", function(req, res){
    req.logout();
+   req.flash("success", "Logged you out!");
    res.redirect("/campgrounds");
 });
 
