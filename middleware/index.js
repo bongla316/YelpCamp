b72abf8ff2c5bf9920ec8948025a1eadebe76403
bongla316 <abdullah.safdar@gmail.com>
2018-09-18 17:42:24 +0100

    Added flash messages for any errors and fixed bugs

--- middleware/index.js
+++ middleware/index.js
@@ -14,18 +14,21 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
    // Check is someone is logged in first and if true, check if user owns the campground
    if(req.isAuthenticated()){
       Campground.findById(req.params.id, function(err, foundCampground){
-         if(err){
+         if(err || !foundCampground){
+            req.flash("error", "Campground not found");
             res.redirect("back");
          } else {
             if (foundCampground.author.id.equals(req.user._id)) {
                next();
             } else {
+               req.flash("error", "You don't have permission to do that");
                res.redirect("back");
             }
          }
       });
 
    } else {
+      req.flash("error", "You need to be logged in to do that");
       res.redirect("back");
    }
 }
@@ -35,17 +38,20 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
    // Check if user logged in first
    if(req.isAuthenticated()){
       Comment.findById(req.params.comment_id, function(err, foundComment){
-         if(err){
+         if(err || !foundComment){
+            req.flash("error", "Comment not found");
             res.redirect("back");
          } else {
             if(foundComment.author.id.equals(req.user._id)){
                next();
             } else {
+               req.flash("error", "You don't have permission to do that");
                res.redirect("back");
             }
          }
       });
    } else {
+      req.flash("error", "You need to be logged in to do that");
       res.redirect("back");
    }
 }
@@ -55,6 +61,8 @@ middlewareObj.isLoggedIn = function(req, res, next){
    if(req.isAuthenticated()){
       return next();
    }
+   // Update flash to access on the next request
+   req.flash("error", "You must be logged in to do that!");
    return res.redirect("/login");
 }
 
